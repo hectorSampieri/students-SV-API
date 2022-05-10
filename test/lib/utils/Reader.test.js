7e2a809ef6983a8c2f383725836177c390d2cc40
hectorSampieri <hoscmx@outlook.com>
2022-05-10 11:47:55 -0500

    Se agrega datos de pruebas independientes del archivo orignal

--- test/lib/utils/Reader.test.js
+++ test/lib/utils/Reader.test.js
@@ -2,7 +2,7 @@ const Reader = require("../../../lib/utils/Reader");
 
 describe("Pruebas de la clase Reader", () => {
     test("1) Leer lista de estudiantes", () => {
-        const students =Reader.readJsonFile("./data/visualpartners.json");
+        const students =Reader.readJsonFile('./test/data/dataTest.json');
         expect(students.length).toBe(51);
     });
 });
